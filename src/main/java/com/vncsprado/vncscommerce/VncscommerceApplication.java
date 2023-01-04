ac87e1a68eb0e30faa7c89126c62792cdad14861
Vinicius Prado <viniciusprado.prado@gmail.com>
2023-01-04 20:51:04 -0300

    Order

---
+++ src/main/java/com/vncsprado/vncscommerce/VncscommerceApplication.java
@@ -0,0 +1,13 @@
+package com.vncsprado.vncscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VncscommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VncscommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vncsprado/vncscommerce/VncscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vncsprado.vncscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VncscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
